411bb39fa7ed74689c4515697ffc413c50a38da8
unknown <patrickmartinez77@gmail.com>
2017-11-14 15:01:06 -0600

    Add account Model

---
+++ src/accountManager/model/AccountModel.java
@@ -0,0 +1,5 @@
+package accountManager.model;
+
+public class AccountModel extends AbstractModel {
+	
+}
--- src/accountManager/model/ModelEvent.java
+++ src/accountManager/model/ModelEvent.java
@@ -8,6 +8,11 @@ import java.awt.event.ActionEvent;
  */
 public class ModelEvent extends ActionEvent {
 	
+	/**
+	 * Added serial version UID per IDE recommendation
+	 */
+	private static final long serialVersionUID = 5939937586052315280L;
+	
 	private int amount;
 	
 	public ModelEvent (Object obj, int id, String message, int amount) {
