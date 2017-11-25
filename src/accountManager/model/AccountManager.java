ea62a0036c3b2a9d1c91425fa0caebdcb3fa4030
martip23 <patrickmartinez77@gmail.com>
2017-11-25 17:25:59 -0600

    Added file reader and scanner in AccountMan

--- src/accountManager/model/AccountManager.java
+++ src/accountManager/model/AccountManager.java
@@ -1,6 +1,10 @@
 package accountManager.model;
 
 import java.util.ArrayList;
+import accountManager.model.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Takes a bank account file and parses file information.
@@ -9,7 +13,7 @@ import java.util.ArrayList;
 public class AccountManager {
 	
 	public final int setLength = 3;
-	ArrayList accountData = new ArrayList();
+	ArrayList<String> accountData = new ArrayList();
 	
 	/**
 	 * Creates array with file information
@@ -17,14 +21,33 @@ public class AccountManager {
 	 * @throws InvalidInputFileException 
 	 */
 	AccountManager(String filename) throws InvalidInputFileException{
-		throw new InvalidInputFileException();
+		File file = new File(filename);
+		
+		try {
+			Scanner sc = new Scanner(file);
+			
+			while (sc.hasNext()) {
+				accountData.add(sc.next());
+			}
+			sc.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
+	/**
+	 * Create AccountModel and return
+	 * @param index Which account to load
+	 * @return AccountModel with parameters
+	 */
 	public AccountModel SelectAccount(int index){
 		//Navigate to first element in selected account
 		int i = ((index-1) * setLength);
-		return (AccountModel(accountData.get(i),
-				accountData.get(++i),
-				accountData.get(++i)));
+		AccountModel model = new AccountModel(accountData.get(i),
+				Integer.parseInt(accountData.get(++i)),
+				Integer.parseInt(accountData.get(++i)));
+		return model;
 	}
 }
