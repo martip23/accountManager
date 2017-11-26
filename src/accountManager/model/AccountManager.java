a67afd87fa3d4db3e8e76e729d10f424ba240c6c
martip23 <patrickmartinez77@gmail.com>
2017-11-25 18:46:29 -0600

    Added file saver and test cases

--- src/accountManager/model/AccountManager.java
+++ src/accountManager/model/AccountManager.java
@@ -1,7 +1,6 @@
 package accountManager.model;
 
 import java.util.ArrayList;
-import accountManager.model.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -14,8 +13,8 @@ import java.util.Scanner;
  */
 public class AccountManager {
 	
-	final int setLength = 3;
-	ArrayList<String> accountData = new ArrayList();
+	public final int setLength = 3;
+	ArrayList<String> accountData = new ArrayList<String>();
 	String filename;
 	
 	/**
@@ -23,8 +22,8 @@ public class AccountManager {
 	 * @param filename
 	 * @throws InvalidInputFileException 
 	 */
-	public AccountManager(String filename) throws InvalidInputFileException{
-		filename = filename;
+	public AccountManager(String fn) throws InvalidInputFileException{
+		filename = fn;
 		File file = new File(filename);
 		
 		try {
@@ -54,26 +53,32 @@ public class AccountManager {
 		return model;
 	}
 	
+	/**
+	 * Saves account state to file, pass in the model that is to be updated
+	 * @param model The model to update.
+	 */
 	public void saveAccount(AccountModel model) {
 		
-		Boolean found;
+		Boolean found = false;
 		int index = 1;
 		
-		while (!found && accountData.size() < index) {
+		while (!found && accountData.size() > index) {
 			if((Integer.parseInt(accountData.get(index)) == model.getID())) {
 				found = true;
 				accountData.set((index + 1), Integer.toString(model.getBalance()));
 			}
+			else index += 3;
 		}
 		File fnew = new File(filename);
 		try {
 			FileWriter fw = new FileWriter(fnew, false);
 			for (int i = 0; i < accountData.size(); i++) {
-				fw.write(accountData + " ");
+				fw.write(accountData.get(i) + " ");
 				if ((i + 1) % setLength == 0) {
-					fw.write("\n");
+					fw.write(System.getProperty("line.separator"));
 				}
 			}
+			fw.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/test/AccountManagerTest.java
+++ src/test/AccountManagerTest.java
@@ -2,15 +2,18 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import accountManager.model.*;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AccountManagerTest {
 	
 
 
 	@Test
-	public void testAccount1() throws InvalidInputFileException {
+	public void testReadAccount1() throws InvalidInputFileException {
 		AccountManager accManager = new AccountManager("accounts.txt");
 		AccountModel model = accManager.selectAccount(1);
 		assertEquals(model.getBalance(), 50000);
@@ -19,12 +22,38 @@ public class AccountManagerTest {
 	}
 	
 	@Test
-	public void testAccount2() throws InvalidInputFileException {
+	public void testReadAccount2() throws InvalidInputFileException {
 		AccountManager accManager = new AccountManager("accounts.txt");
 		AccountModel model = accManager.selectAccount(2);
 		assertEquals(model.getBalance(), 100000);
 		assertEquals(model.getID(), 002);
 		assertEquals(model.getName(), "Rodion");
+	}
+	
+	@Test
+	public void testWriteAccount1() throws InvalidInputFileException {
+		AccountManager accManager = new AccountManager("accounts.txt");		
+		AccountModel model = accManager.selectAccount(2);
+		assertEquals(model.getBalance(), 100000);
+		assertEquals(model.getID(), 002);
+		assertEquals(model.getName(), "Rodion");
+		
+		model.deposit(150000);
+		accManager.saveAccount(model);
+		
+		AccountManager accMan2 = new AccountManager("accounts.txt");
+		model = accManager.selectAccount(2);
+		assertEquals(model.getBalance(), 250000);
+		assertEquals(model.getID(), 002);
+		assertEquals(model.getName(), "Rodion");
+		
+		try {
+			model.withdraw(150000);
+		} catch (OverdrawException e) {
+			e.printStackTrace();
+		}
+		
+		accMan2.saveAccount(model);
 	}
 
 }
